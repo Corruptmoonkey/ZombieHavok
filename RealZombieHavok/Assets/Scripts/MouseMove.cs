8fe9ee78bc7a43253c0f8242c1c3142e19be497f
JayG1212 <jaybgunderson@gmail.com>
2024-10-06 20:26:12 -0500

    New player object and movement scripts
    Was having a lot of issues with the old player object, changed the script and object

--- RealZombieHavok/Assets/Scripts/MouseMove.cs
+++ RealZombieHavok/Assets/Scripts/MouseMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MouseMove : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 500f;
 
     float xRotation = 0f;
     float yRotation = 0f;
@@ -20,12 +20,25 @@ public class MouseMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
+        // Getting Mosue Inputs
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
+        
+        // Look up and down
         xRotation -= mouseY;
+
+
+        // Clamp  rotation
         xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
+       
+
+        // Look left and right
         yRotation += mouseX;
+       
+
+        // Apply rotations to the transform
         transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
     }
 }
