25cd69b63eae0fe72edbf11f5ff12210e534eb88
steve-p-1 <steven.pichelman@mnsu.edu>
2024-10-08 00:25:16 -0500

    l

--- RealZombieHavok/Assets/Scripts/UI/PauseMenu.cs
+++ RealZombieHavok/Assets/Scripts/UI/PauseMenu.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -19,18 +20,12 @@ public class PauseMenu : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            // unlocks cursor to allow it to select.
-            Cursor.lockState = IsPaused ? CursorLockMode.Locked : CursorLockMode.Confined;
-            //disable mouse aiming
-            MouseMove.enabled = !MouseMove.enabled;
-            _PauseMenu.gameObject.SetActive(!IsPaused);
-            IsPaused = !IsPaused;
+           TogglePauseMenu();
         }
     }
     public void Resume()
     {
-        _PauseMenu.gameObject.SetActive(!IsPaused);
-        IsPaused = !IsPaused;
+        TogglePauseMenu();
     }
     public void Options()
     {
@@ -39,5 +34,17 @@ public class PauseMenu : MonoBehaviour
     public void ToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1f;
+    }
+    public void TogglePauseMenu()
+    {
+        Time.timeScale = IsPaused ? 1.0f : 0.0f;
+        Debug.Log(Time.timeScale.ToString());
+        // unlocks cursor to allow it to select.
+        Cursor.lockState = IsPaused ? CursorLockMode.Locked : CursorLockMode.Confined;
+        //disable mouse aiming
+        MouseMove.enabled = IsPaused;
+        _PauseMenu.gameObject.SetActive(!IsPaused);
+        IsPaused = !IsPaused;
     }
 }
