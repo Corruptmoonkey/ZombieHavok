39e2687a331c5f4d8c4865650c51d9a88de5978b
JayG1212 <jaybgunderson@gmail.com>
2024-11-06 15:06:46 -0600

    Reapply "Weapon PickUp"
    This reverts commit 90b219a344f7a16d2b332b8cd79bfc342830dfa9.

--- RealZombieHavok/Assets/Scripts/InteractionManager.cs
+++ RealZombieHavok/Assets/Scripts/InteractionManager.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class InteractionManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static InteractionManager Instance { get; set; }
+    public WeaponScript hoveredWeapon = null;
+  
+
+    private void Awake()
     {
-        
+
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = this;
+            }
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    private void Update()
     {
-        
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject objectHitByRaycast = hit.transform.gameObject;
+            if (objectHitByRaycast.GetComponent<WeaponScript>() && objectHitByRaycast.GetComponent<WeaponScript>().isActiveWeapon == false)
+            {
+                print("Press F to equip");
+                hoveredWeapon = objectHitByRaycast.gameObject.GetComponent<WeaponScript>();
+                
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    WeaponManager.Instance.PickUpWeapon(objectHitByRaycast.gameObject);
+                }
+                
+            }
+          
+
+
+
+        }
     }
 }
+
--- RealZombieHavok/Assets/Scripts/WeaponScript.cs
+++ RealZombieHavok/Assets/Scripts/WeaponScript.cs
@@ -29,7 +29,7 @@ public class WeaponScript : MonoBehaviour
     public Vector3 spawnRotation;
     public Vector3 spawnSize;
 
-    private Animator animator;
+    public Animator animator;
 
     public bool isADS;
 
@@ -59,6 +59,7 @@ public class WeaponScript : MonoBehaviour
         burstBulletsLeft = bulletsPerBurst;
         bulletsLeft = magazineSize;
         animator = GetComponent<Animator>();
+        isActiveWeapon = false;
     }
 
 
